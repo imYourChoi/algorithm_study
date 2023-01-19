a7096e1ac724b5d8032b3c37bbcacfa3e60b7713
imYourChoi <jeehun@kaist.ac.kr>
2023-01-20 01:37:04 +0900

    Update: 양팔저울

--- dynamic_programming/scale.py
+++ dynamic_programming/scale.py
@@ -5,20 +5,35 @@ weights = [0] + list(map(int, input().split()))
 num_gem = int(input())
 gems = list(map(int, input().split()))
 
-dp = [{} for _ in range(num_weight+1)]
+# dp = [{} for _ in range(num_weight+1)]
 
-for i in range(1,num_weight+1):
-    for j in range(-sum(weights)-1,sum(weights)+1):
-        weight = weights[i]
+# for i in range(1,num_weight+1):
+#     for j in range(-sum(weights)-1,sum(weights)+1):
+#         weight = weights[i]
 
-        dp[i].update(dp[i-1])
+#         dp[i].update(dp[i-1])
         
-        dp[i][weight] = 1
-        dp[i][-weight] = 1
+#         dp[i][weight] = 1
+#         dp[i][-weight] = 1
 
-        if v:=dp[i-1].get(j-weight,0) | dp[i-1].get(j+weight,0):
-            dp[i][j] = v
-print(dp)
+#         if v:=dp[i-1].get(j-weight,0) | dp[i-1].get(j+weight,0):
+#             dp[i][j] = v
+# print(dp)
+# for gem in gems:
+#     print("Y" if dp[-1].get(gem,0) else "N", end=" ")
+# print()
+
+dp = set()
+
+for weight in weights:
+    new = set({weight})
+
+    for num in dp:
+        new.add(abs(num-weight))
+        new.add(num+weight)
+    
+    dp = dp.union(new)
+
 for gem in gems:
-    print("Y" if dp[-1].get(gem,0) else "N", end=" ")
+    print("Y" if gem in dp else "N", end=" ")
 print()
