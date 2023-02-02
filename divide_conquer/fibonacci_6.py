6a3ffa6264bc67ed2d899889f9726a309b800520
imYourChoi <jeehun@kaist.ac.kr>
2023-02-02 16:50:19 +0900

    피보나치 6 w/ 분할 정복

--- divide_conquer/fibonacci_6.py
+++ divide_conquer/fibonacci_6.py
@@ -1,14 +1,27 @@
 # https://www.acmicpc.net/problem/11444
 
 n = int(input())
+divide = 1000000007
+
+def multiply(first, second):
+    return [
+        [(first[0][0]*second[0][0] + first[0][1]*second[1][0]) % divide,
+        (first[0][0]*second[0][1] + first[0][1]*second[1][1]) % divide],
+        [(first[1][0]*second[0][0] + first[1][1]*second[1][0]) % divide,
+        (first[1][0]*second[0][1] + first[1][1]*second[1][1]) % divide],
+    ]
 
 def fibonacci(n):
-    n1, n2 = 0, 1
-    for i in range(n):
-        temp = (n1 + n2) % 1000000007
-        n1 = n2
-        n2 = temp
+    if n == 0:
+        return [[0,0]]
+    if n == 1:
+        return [[1,1],[1,0]]
 
-    return n1 % 1000000007
+    matrix = fibonacci(n//2)
+    multiple = multiply(matrix,matrix)
 
-print(fibonacci(n))
+    if n % 2:
+        return multiply(multiple, [[1,1],[1,0]])
+    return multiple
+
+print(fibonacci(n)[0][1])
