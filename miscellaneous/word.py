b70c2e2361612cc3039e35d2a3d7502e8e102ce4
imYourChoi <jeehun@kaist.ac.kr>
2023-02-04 23:31:57 +0900

    단어 공부

---
+++ miscellaneous/word.py
@@ -0,0 +1,6 @@
+word = input()
+d = {}
+for char in word.lower():
+    d[char] = d.get(char,0)+1
+s = sorted(list(d.items()),key=lambda x:x[1])
+print("?" if len(s) > 1 and s[-2][1] == s[-1][1] else s[-1][0].upper())
