00488c6f2137bc50166e0964909eb9efc701667d
imYourChoi <jeehun@kaist.ac.kr>
2023-02-08 17:47:03 +0900

    적록색약

---
+++ graph/rg_color_weak.py
@@ -0,0 +1,35 @@
+# https://www.acmicpc.net/problem/10026
+
+n = int(input())
+colors = [[*input()] for _ in range(n)]
+visited = [[False] * n for _ in range(n)]
+
+def bfs(y,x):
+    color = colors[y][x]
+    if color in ["R", "G"]:
+        colors[y][x] = "W"
+    for dy,dx in [(-1,0), (1,0), (0,1), (0,-1)]:
+        Y, X = y+dy , x+dx
+        if not 0<=Y<n or not 0<=X<n or visited[Y][X]:
+            continue
+        if color == colors[Y][X]:
+            visited[Y][X] = True
+            bfs(Y,X)
+
+normal = 0
+for y in range(n):
+    for x in range(n):
+        if not visited[y][x]:
+            bfs(y,x)
+            normal += 1
+# print(*colors, sep="\n")
+weak = 0
+visited = [[False] * n for _ in range(n)]
+for y in range(n):
+    for x in range(n):
+        if not visited[y][x]:
+            bfs(y,x)
+            weak += 1
+
+print(normal, weak)
+# print(visited)
