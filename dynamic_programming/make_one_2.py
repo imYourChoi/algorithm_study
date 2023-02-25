abdfabd2bb97ae173042559ca65f71459685a2c6
imYourChoi <jeehun@kaist.ac.kr>
2023-02-25 18:28:27 +0900

    1로 만들기 2

---
+++ dynamic_programming/make_one_2.py
@@ -0,0 +1,32 @@
+# https://www.acmicpc.net/problem/12852
+
+n = int(input())
+
+def make_one():
+    visited = [False] * (n+1)
+    dp = [[n]]
+    while dp:
+        new = []
+        for each in dp:
+            if not each[-1] % 3:
+                if not visited[v:=each[-1]//3]:
+                    new.append(each[:] + [v])
+                    visited[v] = True
+                    if v == 1:
+                        return new[-1]
+            if not each[-1] % 2:
+                if not visited[v:=each[-1]//2]:
+                    new.append(each[:] + [v])
+                    visited[v] = True
+                    if v == 1:
+                        return new[-1]
+            if not visited[v:=each[-1]-1]:
+                new.append(each[:] + [v])
+                visited[v] = True
+                if v == 1:
+                    return new[-1]
+        dp = new
+
+answer = make_one()
+print(len(answer)-1)
+print(*answer)
