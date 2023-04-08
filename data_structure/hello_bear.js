96e7aee69df590a3960f426f8adc20d7ec3866b6
imYourChoi <jeehun@kaist.ac.kr>
2023-04-08 18:19:43 +0900

    인사성 밝은 곰곰이

---
+++ data_structure/hello_bear.js
@@ -0,0 +1,25 @@
+// https://www.acmicpc.net/problem/25192
+
+const fs = require("fs");
+// const [n, enter, ...lines] = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const [n, enter, ...lines] = fs
+  .readFileSync(__dirname + "/input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+const set = new Set();
+let answer = 0;
+for (let line of lines) {
+  if (line === "ENTER") {
+    set.clear();
+    continue;
+  } else if (set.has(line)) {
+    continue;
+  } else {
+    set.add(line);
+    answer++;
+  }
+}
+
+console.log(answer, __dirname);
