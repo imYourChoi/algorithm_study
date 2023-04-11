b8c86b2b8eb16ace38365feb9a5cd286f80a9718
imYourChoi <jeehun@kaist.ac.kr>
2023-04-11 20:58:36 +0900

    Refactor: separate file for input method

--- two_pointer/sequence.js
+++ two_pointer/sequence.js
@@ -1,15 +1,9 @@
 // https://www.acmicpc.net/problem/2559
 
-const fs = require("fs");
-// const [n, input] = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
-const [n, input] = fs
-  .readFileSync(__dirname + "/../input.txt")
-  .toString()
-  .trim()
-  .split("\n");
+const input = require("../input.js");
 
-const [N, K] = n.split(" ").map((n) => +n);
-const numbers = input.split(" ").map((n) => +n);
+const [N, K] = input().split(" ").map(Number);
+const numbers = input().split(" ").map(Number);
 
 let index = 0;
 let temp = 0;
@@ -17,7 +11,7 @@ let answer = -Number.MAX_SAFE_INTEGER;
 if (N == K) {
   answer = numbers.reduce((acc, cur) => acc + cur, 0);
 } else {
-  numbers.forEach((num) => {
+  numbers.forEach(num => {
     temp += num;
     if (index == K - 1) answer = temp;
     else if (index >= K) {
