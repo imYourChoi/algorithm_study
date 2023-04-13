d71f6a577c9dd3680a6f9326f1167782f7be292e
imYourChoi <jeehun@kaist.ac.kr>
2023-04-13 23:07:45 +0900

    인간-컴퓨터 상호작용 w/ pypy3

---
+++ miscellaneous/hci.py
@@ -0,0 +1,21 @@
+# https://www.acmicpc.net/problem/16139
+
+s = input()
+answer = []
+acc = [[0 for i in range(len(s))] for i in range(26)]
+set = set()
+
+for _ in range(int(input())):
+    alpha, start, end = input().split();
+    start, end = int(start), int(end)
+    if alpha in set:
+        answer.append(acc[ord(alpha) - 97][end] - (acc[ord(alpha) - 97][start - 1] if start else 0))
+    else:
+        set.add(alpha)
+        array = acc[ord(alpha)-97]
+        for i, c in enumerate(s):
+            if i: array[i] = array[i-1] + (c == alpha)
+            else: array[0] = 0 + (c == alpha)
+        answer.append(array[end] - (array[start-1] if start else 0))
+
+print(*answer, sep="\n")
