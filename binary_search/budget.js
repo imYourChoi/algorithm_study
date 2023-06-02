8e8e87af3048a1950013c1807f95db152f7c9f3e
imYourChoi <jeehun@kaist.ac.kr>
2023-06-02 20:10:24 +0900

    예산

---
+++ binary_search/budget.js
@@ -0,0 +1,35 @@
+// https://www.acmicpc.net/problem/2512
+
+const stdin = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : __dirname + "/../input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+const N = +input();
+const nums = input()
+  .split(" ")
+  .map(Number)
+  .sort((a, b) => a - b);
+const budget = +input();
+// const sum = nums.reduce((a, b) => a + b, 0);
+
+let start = 0,
+  end = nums.reduce((a, b) => Math.max(a, b));
+
+while (start <= end) {
+  const mid = Math.floor((start + end) / 2);
+  let total = 0;
+  for (let num of nums) {
+    if (num > mid) total += mid;
+    else total += num;
+  }
+  if (total <= budget) start = mid + 1;
+  else end = mid - 1;
+}
+
+console.log(end);
