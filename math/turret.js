8ad165a35f5012d3c0f6601d3d6bd4d2bbb28143
imYourChoi <jeehun@kaist.ac.kr>
2023-06-23 16:49:36 +0900

    Fix: 터렛 재채점

--- math/turret.js
+++ math/turret.js
@@ -15,7 +15,7 @@ const T = +input();
 for (let i = 0; i < T; i++) {
   const [x1, y1, r1, x2, y2, r2] = input().split(" ").map(Number);
   const distance = Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
-  if (distance === 0 && r1 === r2) console.log(-1);
+  if (distance === 0 && r1 === r2 && r1 !== 0) console.log(-1);
   else if (Math.abs(r1 - r2) === distance || r1 + r2 === distance) console.log(1);
   else if (Math.abs(r1 - r2) < distance && distance < r1 + r2) console.log(2);
   else console.log(0);
