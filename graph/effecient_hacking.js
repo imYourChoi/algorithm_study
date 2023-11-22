389cd379f915ce5975681680ddc0b968a0072b61
imYourChoi <jeehun@kaist.ac.kr>
2023-11-22 23:32:02 +0900

    Fix: (시간 초과) 효율적인 해킹

--- graph/effecient_hacking.js
+++ graph/effecient_hacking.js
@@ -11,35 +11,27 @@ const input = (() => {
 })();
 
 const [N, M] = input().split(" ").map(Number);
-const graph = Array.from({ length: N + 1 }, () => new Set());
+const graph = Array.from({ length: N + 1 }, () => []);
 const count = Array(N + 1).fill(0);
 
 for (let i = 0; i < M; i++) {
   const [A, B] = input().split(" ").map(Number);
-  graph[B].add(A);
+  graph[B].push(A);
 }
 
 for (let i = 1; i <= N; i++) {
-  count[i] = bfs(i);
+  const visited = Array(N + 1).fill(false);
+  count[i] = dfs(i, visited);
 }
 
-function bfs(node) {
-  const visited = Array(N + 1).fill(false);
+function dfs(node, visited) {
   visited[node] = true;
   let value = 1;
-  let queue = [node];
-  while (queue.length) {
-    let temp = [];
-    for (let i = 0; i < queue.length; i++) {
-      let current = queue[i];
-      for (let computer of graph[current]) {
-        if (visited[computer]) continue;
-        visited[computer] = true;
-        value += 1;
-        temp.push(computer);
-      }
+  for (let i = 0; i < graph[node].length; i++) {
+    const neighbor = graph[node][i];
+    if (!visited[neighbor]) {
+      value += dfs(neighbor, visited);
     }
-    queue = temp;
   }
   return value;
 }
