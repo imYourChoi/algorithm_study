ad59279ba97de02727185813b7e42d75510a256c
imYourChoi <jeehun@kaist.ac.kr>
2025-01-02 21:19:11 +0900

    Fix: 최소 스패닝 트리 (재채점: 중복 간선 처리)

--- graph/min_spanning_tree/min_spanning_tree.py
+++ graph/min_spanning_tree/min_spanning_tree.py
@@ -1,28 +1,35 @@
 # https://www.acmicpc.net/problem/1197
+# https://gmlwjd9405.github.io/2018/08/28/algorithm-mst.html
+
+# 프림 알고리즘
 
 # import heapq
 # import sys
 # input = sys.stdin.readline
 
-# V,E = map(int, input().split())
+# V, E = map(int, input().split())
 # graph = [{} for _ in range(V+1)]
 
 # for _ in range(E):
-#     a,b,c = map(int, input().split())
-#     graph[a][b] = c
-#     graph[b][a] = c
+#     a, b, c = map(int, input().split())
+#     # 중복된 간선이 있을 가능성을 유의할 것!
+#     graph[a][b] = min(graph[a].setdefault(b, c), c)
+#     graph[b][a] = min(graph[b].setdefault(a, c), c)
 
 # visited = [False] * (V+1)
 
-# heap = [(0,1)]
+# heap = [(0, 1)]
 # answer = 0
+# count = 0
 
-# while heap:
+# while count < V:
 #     close_dist, close_node = heapq.heappop(heap)
+
 #     if visited[close_node]:
 #         continue
 #     visited[close_node] = True
 #     answer += close_dist
+#     count += 1
 
 #     for node, dist in graph[close_node].items():
 #         if visited[node]:
@@ -31,6 +38,7 @@
 
 # print(answer)
 
+# 크루스칼 알고리즘
 # https://velog.io/@yoopark/baekjoon-1197
 
 import sys
@@ -99,3 +107,14 @@ print(answer)
 # 2 5 7
 # 3 5 8
 # 4 5 9
+
+# 7 9
+# 1 2 8
+# 2 3 17
+# 3 4 27
+# 4 5 29
+# 5 6 21
+# 5 7 23
+# 7 2 10
+# 6 1 15
+# 7 4 25
