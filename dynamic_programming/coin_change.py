4af312d774fa29da53b22a45ed8a74948a1ea0cc
imYourChoi <jeehun@kaist.ac.kr>
2026-01-24 23:39:58 +0900

    Coin Change

---
+++ dynamic_programming/coin_change.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/coin-change/description/
+
+from typing import List
+
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [float('inf') for _ in range(amount + 1)]
+        dp[0] = 0
+
+        coins.sort()
+        coins.reverse()
+
+        for coin in coins:
+            for i in range(coin, amount + 1):
+                dp[i] = min(dp[i], dp[i - coin] + 1)
+
+        return dp[-1] if dp[-1] != float('inf') else -1
