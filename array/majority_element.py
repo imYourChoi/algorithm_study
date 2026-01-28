a123f46c5a9d5b1f1445727653551a622d650c1d
imYourChoi <jeehun@kaist.ac.kr>
2026-01-28 11:21:10 +0900

    Majority Element

---
+++ array/majority_element.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/majority-element/description/
+
+from collections import Counter
+from typing import List
+
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        c = Counter(nums)
+        return c.most_common(1)[0][0]
