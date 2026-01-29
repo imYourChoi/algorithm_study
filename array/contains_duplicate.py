2b97bf14de941fd6eed756077850d850bc69c179
imYourChoi <jeehun@kaist.ac.kr>
2026-01-29 11:01:13 +0900

    Contains Duplicate

---
+++ array/contains_duplicate.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/contains-duplicate/description/
+
+from typing import List
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        count = {}
+
+        for num in nums:
+            if num in count:
+                return True
+            else:
+                count[num] = 1
+
+        return False
