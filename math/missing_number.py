676627e2df13cb19ce4961133174f569f674593b
imYourChoi <jeehun@kaist.ac.kr>
2026-02-10 22:09:46 +0900

    Missing Number

---
+++ math/missing_number.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/missing-number/description/
+
+from typing import List
+
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        return sum(range(len(nums)+1)) - sum(nums)
