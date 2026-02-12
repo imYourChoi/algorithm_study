2504f961b88680624acd744e6d104df202a67075
imYourChoi <jeehun@kaist.ac.kr>
2026-02-12 16:13:28 +0900

    Reverse Bits

---
+++ binary/reverse_bits.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/reverse-bits/description/
+
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        result = 0
+        for i in range(32):
+            bit = n & 1
+            result = (result << 1) | bit
+            n >>= 1
+        return result
