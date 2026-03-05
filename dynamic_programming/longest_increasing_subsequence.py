afb6df355bc3d1e9b16b8808b7f8fdb32271f613
imYourChoi <jeehun@kaist.ac.kr>
2026-03-05 11:09:11 +0900

    Longest Increasing subsequence

---
+++ dynamic_programming/longest_increasing_subsequence.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/longest-increasing-subsequence/description/
+
+import bisect
+from typing import List
+
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        dp = []
+
+        for num in nums:
+            index = bisect.bisect_left(dp, num)
+
+            if index == len(dp):
+                dp.append(num)
+            else:
+                dp[index] = num
+
+        return len(dp)
